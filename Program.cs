69b4530ea7b83d9696cf9bccec63e2cb9b96b82a
Matheus <matheeuzinhoo282@gmail.com>
2026-05-03 20:54:44 -0300

    doador sangue

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿﻿
+Console.WriteLine("Qual a sua idade:");
+decimal idadeDoador  = Convert.ToDecimal(Console.ReadLine());
+
+if (idadeDoador >= 18 && idadeDoador <= 67)
+{
+    Console.WriteLine("Você pode doar sangue.");
+}
+else
+    Console.WriteLine("Você não pode doar sangue.");
